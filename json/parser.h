751cd465c8588a502c76ee175d31e65c671bddc0
harry_git <280971246@qq.com>
2023-12-15 18:27:59 +0800

    day02 新建测试对象

---
+++ json/parser.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <string>
+using namespace std;
+
+#include "json.h"
+
+namespace yazi {
+namespace json {
+
+class Parser
+{
+public:
+    Parser();
+    ~Parser();
+
+    void load(const string & str);
+    Json parse();
+
+private:
+    void skip_white_spaces();
+    char get_next_token();
+
+    Json parse_null();
+    Json parse_bool();
+    Json parse_number();
+    Json parse_string();
+    Json parse_arr();
+    Json parse_object();
+
+
+    bool in_range(long x, long lower, long upper)
+    {
+        return (x >= lower && x <= upper);
+    }
+
+private:
+    string m_str;
+    size_t m_idx;
+};
+
+}}
